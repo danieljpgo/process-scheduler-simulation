9eb9436a4d6d5fa79c8651265b613d04aa0ff357
Daniel Jorge <djpgonzaga@gmail.com>
2019-10-09 23:58:27 -0300

    creating a process queue class

--- escalonador-aed/escalonador-aed/Processo.fila.cs
+++ escalonador-aed/escalonador-aed/Processo.fila.cs
@@ -3,10 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Threading;
 
 namespace escalonador_aed
 {
     class ProcessoFila
     {
+        public ProcessoUnidade Anterior { get; set; }
+        public ProcessoUnidade Posterior { get; set; }
+        public int ContadorProcesso { get; set; }
+
+        // @TODO Ajustar aqui
+        // Classe Mutex, será usada para controlar a execução por múltiplas threads
+        // https://docs.microsoft.com/en-us/dotnet/api/system.threading.mutex?view=netframework-4.8
+        // http://www.macoratti.net/16/02/c_mutex1.htm
+        Mutex mutex = new Mutex();
+
+        // Construtor
+        public ProcessoFila()
+        {
+            ProcessoUnidade sentinela = new ProcessoUnidade();
+            Anterior = sentinela;
+            Posterior = sentinela;
+            ContadorProcesso = 0;
+        }
+
+        // Checar se a fila está vazia
+        public bool FilaVazia()
+        {
+            if (Anterior == Posterior)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        // Procurar uma Unidade de Processo na fila de Processos (findIndex na mão)
+        public Processo ProcuraProcesso(int indice)
+        {
+            if (!FilaVazia())
+            {
+                mutex.WaitOne();
+
+                // Verificação para caso o indice seja maior que o possível
+                if (indice >= ContadorProcesso)
+                {
+                    mutex.ReleaseMutex();
+                    return null;
+                }
+
+                ProcessoUnidade aux = Posterior.Proximo;
+
+                // Avança o ponteiro até chegar no indice que deseja obter o processo
+                for (int cont = 0; cont < indice; cont++)
+                {
+                    aux = aux.Proximo;
+                }
+
+                mutex.ReleaseMutex();
+                return aux.Processo;
+            }
+            return null;
+        }
     }
 }
--- escalonador-aed/escalonador-aed/Processo.unidade.cs
+++ escalonador-aed/escalonador-aed/Processo.unidade.cs
@@ -8,5 +8,19 @@ namespace escalonador_aed
 {
     class ProcessoUnidade
     {
+        // Atributos
+        public Processo Processo { get; set; }
+        public ProcessoUnidade Proximo { get; set; }
+
+        // Construtor
+        public ProcessoUnidade(Processo processo)
+        {
+            Processo = processo;
+        }
+
+        public ProcessoUnidade()
+        {
+
+        }
     }
 }
